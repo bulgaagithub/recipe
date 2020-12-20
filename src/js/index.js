5e675523b99e0b5280771e36bd89c38a5f655514
Erdenebulgan <erdenebulgandeveloper@gmail.com>
2020-12-20 23:35:14 +0800

    lesson 115

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,8 @@
 // ./ current folder
-require('@babel/polyfill');
-import axios from "axios";
 
-async function doSearch(search) {
-    // Lesson 114.
-    try {
-        let result = await axios('https://forkify-api.herokuapp.com/api/search?q=' + search);
-        // console.log(result.data.recipes);
-    
-        const recipes = result.data.recipes;
-        console.log(recipes);
-    
-        result = await axios('https://forkify-api.herkuapp.com/api/get?rId=' + recipes[1].recipe_id);
-    
-        console.log(result);
-    } catch(error) {
-        alert('Алдаа гарлаа: ' + error);
-    }
-    
-}
+import Search from "./model/Search";
 
-doSearch('pizza');
+let search = new Search('pasta');
+
+search.doSearch().then(r => console.log(r));
+
--- src/js/model/Search.js
+++ src/js/model/Search.js
@@ -1,3 +1,22 @@
-let query = 'pizza';
+require('@babel/polyfill');
+import axios from "axios";
 
-export default query;
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async doSearch() {
+    // Lesson 115.
+    try {
+      let result = await axios(
+        "https://forkify-api.herokuapp.com/api/search?q=" + this.query
+      );
+
+      this.result = result.data;
+      return this.result; // Remember this function will return promise 
+    } catch (error) {
+      alert("Алдаа гарлаа: " + error);
+    }
+  }
+}
